583790322e2f319c2790d42b83f37293064a5874
BePower1001 <adrian.pytlak@outlook.com>
2025-11-05 18:21:40 +0000

    More code, using strip and title parameters

--- hello.py
+++ hello.py
@@ -1,9 +1,25 @@
-#Ask user for their name
-name = input("Whats your name? ")
-#Say hello to the user
-print("Hello,", name)
+# #Ask user for their name
+# name = input("Whats your name? ")
+# #Say hello to the user
+# print("Hello,", name)
+#
+#
+# """
+# Comment testing
+# """
+#
+#
+# #Asking user again
+# name2=input("Whats your name? ")
+# print(f"Hello, {name2}")
 
 
-"""
-Comment testing 
-"""
+#
+# #Remove whitespace from string
+# name = input("What is your name? ")
+# name = name.strip().title()
+# print(f"Hello, {name}")
+
+#Even better way to write a code using 1 line of code
+name = input("What is your name? ").strip().title()
+print(f"Hello, {name}")
