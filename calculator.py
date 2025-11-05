f0c0cc62f35c6f45a69e0fd9c229a1abfae93e72
BePower1001 <adrian.pytlak@outlook.com>
2025-11-05 18:38:01 +0000

    Calculator 2 ways to create addition

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# x = 1
+# y = 2
+# z = x + y
+# print(z)
+
+# #We need to convert string into integer in line 10
+# x = input("What is x? ")
+# y = input("What is y? ")
+#
+# z = int(x) + int(y)
+# print(z)
+
+
+#Even now we can do something else with integer
+
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+print(x + y)
